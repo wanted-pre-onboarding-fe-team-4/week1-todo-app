f6c41c604db3cacfae6937b03811d25b88569b88
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-22 22:35:11 +0900

    fix: change export default to export

--- src/hooks/useLoggedIn.tsx
+++ src/hooks/useLoggedIn.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const useLoggedIn = () => {
+export const useLoggedIn = () => {
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -13,5 +13,3 @@ const useLoggedIn = () => {
     if (authUrls.includes(url) && token) navigate('/todo')
   }, [])
 }
-
-export default useLoggedIn
