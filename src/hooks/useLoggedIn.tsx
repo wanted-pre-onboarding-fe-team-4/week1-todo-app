1bb162437f6d4ac1430ed160068c3c9d31576f4e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-02-22 22:28:37 +0900

    feat: add useLoggedIn for check login status

---
+++ src/hooks/useLoggedIn.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const useLoggedIn = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const url = window.location.pathname
+    const authUrls = ['/signup', '/signin']
+    const token = localStorage.getItem('access_token')
+
+    if (!authUrls.includes(url) && !token) navigate('/signin')
+    if (authUrls.includes(url) && token) navigate('/todo')
+  }, [])
+}
+
+export default useLoggedIn
