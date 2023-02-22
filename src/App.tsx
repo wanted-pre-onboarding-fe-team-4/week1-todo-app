a05c53f3d0c581497c9e324cd6afbcc9f977922b
SongNoin <as5427072@gmail.com>
2023-02-23 00:36:20 +0900

    fix App.tsx 의 fragment 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import AppRouter from 'components/commons/routes/AppRouter'
 
 function App() {
-  return (
-    <>
-      <AppRouter />
-    </>
-  )
+  return <AppRouter />
 }
 
 export default App
