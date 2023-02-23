3673cf344b5797e0cb7d22a11ba614217350752f
Seongbin Kim <seongbin9786@gmail.com>
2023-02-23 23:36:53 +0900

    [#28] 회원가입 페이지 개발  (#30)
    * feat(SignUp Page): API 모듈화
    
    * feat(SignUp Page): API 모듈화 개선 - onSuccess, onFailure 추가
    
    * feat(SignUp Page): 공통 인풋, 버튼에 필요한 props 추가
    
    * feat(SignUp Page): 입력폼 상태, Validation, 리다이렉션 구현

--- src/components/commons/inputs/CommonInput.tsx
+++ src/components/commons/inputs/CommonInput.tsx
@@ -2,39 +2,50 @@ import styled from 'styled-components'
 import { colors } from '../styles/colors'
 
 interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+  type?: string
   onChange: React.ChangeEventHandler<HTMLInputElement>
   placeholder?: string
   backgroundColor?: string
   width?: number
-  height?: number
+  height?: number | string
+  fontSize?: number | string
+  testId?: string
 }
 
 /**
  * 공통인풋
  *
  * 기타 인풋 속성 넣을 수 있습니다.
+ * @param type - input type (기본값 text)
  * @param onChange - onChange 함수 (required)
  * @param placeholder - placeholder
- * @param backgroundColor - 인풋 배경색 (기본 값 white)
+ * @param backgroundColor - 인풋 배경색 (기본값 white)
  * @param width - 인풋 px 넓이 (기본값 100%)
- * @param height - 인풀 px 높이 (기본값 100%)
+ * @param height - 인풋 px 높이 (기본값 100%)
+ * @param fontSize - 폰트 크기 (기본값 16px)
+ * @param testId - data-testid 값
  */
 
 export const CommonInput = (props: InputProps) => {
   return (
     <InputWrap
+      {...props}
+      type={props.type || 'text'}
       backgroundColor={props.backgroundColor}
       width={props.width}
       height={props.height}
+      fontSize={props.fontSize}
       onChange={props.onChange}
       placeholder={props.placeholder}
+      data-testid={props.testId}
     />
   )
 }
 
 const InputWrap = styled.input<{
   width?: number
-  height?: number
+  height?: number | string
+  fontSize?: number | string
   backgroundColor?: string
 }>`
   width: ${(props) => (props.width ? props.width + 'px' : '100%')};
@@ -46,6 +57,6 @@ const InputWrap = styled.input<{
   background-color: ${(props) =>
     props.backgroundColor ? props.backgroundColor : colors.white};
 
-  font-size: 14px;
+  font-size: ${(props) => props.fontSize || '16px'};
   font-weight: normal;
 `
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,75 @@
+import { useState, ComponentProps } from 'react'
+import { useSignUpApi } from 'api/signUpApi'
+import { CommonButton } from 'components/commons/buttons/CommonButton'
+import { useLoggedIn } from 'hooks/useLoggedIn'
+import { SignUpInput, Wrapper } from './style'
+
+type OnChangeHandler = ComponentProps<'input'>['onChange']
+type OnKeyDownHandler = ComponentProps<'input'>['onKeyDown']
+type OnClickHandler = ComponentProps<'button'>['onClick']
+
+export const SignUpPage = () => {
+  useLoggedIn()
+
+  const signupApi = useSignUpApi()
+
+  const [email, setEmail] = useState('')
+  const [emailIsValid, setEmailIsvalid] = useState(false)
+  const [password, setPassword] = useState('')
+  const [passwordlIsValid, setPasswordlIsvalid] = useState(false)
+  const submitAvailable = emailIsValid && passwordlIsValid
+
+  const signup = async () => {
+    await signupApi({ email, password })
+  }
+
+  const handleEmailChange: OnChangeHandler = (e) => {
+    const newValue = e.target.value
+    setEmail(newValue)
+    setEmailIsvalid(newValue.includes('@'))
+  }
+
+  const handlePasswordChange: OnChangeHandler = (e) => {
+    const newValue = e.target.value
+    setPassword(newValue)
+    setPasswordlIsvalid(newValue.length >= 8)
+  }
+
+  const handleSubmit: OnClickHandler = () => {
+    signup()
+  }
+
+  const handleEnter: OnKeyDownHandler = (e) => {
+    if (e.key === 'Enter') {
+      signup()
+    }
+  }
+
+  return (
+    <Wrapper>
+      <SignUpInput
+        autoFocus={true}
+        testId='email-input'
+        type='text'
+        placeholder='이메일'
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <SignUpInput
+        testId='password-input'
+        type='password'
+        placeholder='비밀번호'
+        value={password}
+        onChange={handlePasswordChange}
+        onKeyDown={handleEnter}
+      />
+      <CommonButton
+        testId='signup-button'
+        text='회원가입'
+        onClick={handleSubmit}
+        disabled={!submitAvailable}
+        height={80}
+      />
+    </Wrapper>
+  )
+}
