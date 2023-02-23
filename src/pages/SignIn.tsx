b038998ebc498fc891e5fb119a6e2fd2781e278a
sehyeon <hyeon4687@naver.com>
2023-02-23 21:14:32 +0900

    feat : 회원가입 로그인 custom-hook 호출하여 구현

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,24 @@
+import { useQuery } from 'hooks/useQuery'
+import { useState } from 'react'
+
 export const SignInPage = () => {
-  return <div>로그인</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { query: signin } = useQuery({
+    method: 'post',
+    url: `/auth/signin`,
+  })
+
+  const onClickSignin = async () => {
+    await signin({ email, password })
+  }
+
+  return (
+    <div>
+      <input onChange={(e) => setEmail(e.target.value)} />
+      <input onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={() => onClickSignin()}>로그인</button>
+    </div>
+  )
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,24 @@
+import { useQuery } from 'hooks/useQuery'
+import { useState } from 'react'
+
 export const SignUpPage = () => {
-  return <div>회원가입</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { query: signup } = useQuery({
+    method: 'post',
+    url: `/auth/signup`,
+  })
+
+  const onClickSignup = async () => {
+    await signup({ email, password })
+  }
+
+  return (
+    <div>
+      <input onChange={(e) => setEmail(e.target.value)} />
+      <input onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={() => onClickSignup()}>회원가입</button>
+    </div>
+  )
 }
