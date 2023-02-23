4d3e38ef9e4766e335bd5104d491f30aacb4372f
Seongbin Kim <seongbin9786@gmail.com>
2023-02-23 23:30:04 +0900

    feat(SignUp Page): API 모듈화 개선 - onSuccess, onFailure 추가

--- src/api/signUpApi.ts
+++ src/api/signUpApi.ts
@@ -1,9 +1,25 @@
 import { useQuery } from 'hooks/useQuery'
+import { useNavigate } from 'react-router-dom'
 
 export const useSignUpApi = () => {
+  const navigate = useNavigate()
+  const redirectToSignIn = () => {
+    navigate('/signin')
+  }
+
+  const handleSignUpSuccess = () => {
+    redirectToSignIn()
+  }
+
+  const handleSignUpFailure = () => {
+    alert('중복된 이메일입니다.')
+  }
+
   const { query } = useQuery({
     method: 'post',
     url: `/auth/signup`,
+    onSuccess: handleSignUpSuccess,
+    onFailure: handleSignUpFailure,
   })
   return query
 }
