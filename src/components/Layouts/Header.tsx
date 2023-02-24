cc434b993ca0bdae8908c09e7de75e61427b89b8
hyemin33 <joliie@naver.com>
2023-02-24 21:33:10 +0900

    feat: 로그아웃 버튼 추가

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,14 +1,35 @@
 import { colors } from 'components/commons/styles/colors'
 import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom'
 
 export const Header = () => {
+  const navigate = useNavigate()
+  const token = localStorage.getItem('access_token')
+  const handleLogout = () => {
+    localStorage.removeItem('access_token')
+    navigate('/signin')
+  }
+
   return (
     <Wrap>
       <div>Wanted Todo</div>
+      {token && <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>}
     </Wrap>
   )
 }
 
+const LogoutButton = styled.button`
+  background: #fff;
+  color: ${colors.main};
+  border: none;
+  padding: 3px 10px;
+  border-radius: 5px;
+  position: absolute;
+  right: 100px;
+  bottom: 10px;
+  font-weight: bold;
+  cursor: pointer;
+`
 const Wrap = styled.div`
   width: 100%;
   min-height: 100px;
@@ -17,6 +38,7 @@ const Wrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 
   font-size: 24px;
   font-weight: 600;
