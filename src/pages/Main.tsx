a4db1eab13ae27175e8e0465a1e8e97206708f7c
Wooseong Kim <kimwooseong8313@gmail.com>
2023-02-24 20:36:02 +0900

    feat(Main Page) : singin 페이지로 이동

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export const MainPage = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/signin')
+  }, [])
   return <div>메인</div>
 }
